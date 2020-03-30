5a55f2be3e70b5c9cca5c142b3d9ae7d8238db34
Florin Gogianu <florin.gogianu@gmail.com>
2020-03-30 19:57:02 +0000

    Add conv feature extractor.

--- main.py
+++ main.py
@@ -60,9 +60,7 @@ class SparseAttention(nn.Module):
         h = torch.bmm(torch.matmul(x, wk), torch.matmul(x, wq).transpose(1, 2))
         A = torch.softmax(self.scale * h, dim=-1).sum(-2)
         idxs = torch.argsort(A, descending=True)[:, : self.topk]
-        return x.gather(
-            1, idxs.view(N * T, self.topk, 1).expand(N * T, self.topk, d_in)
-        ).view(N, T, self.topk, d_in)
+        return idxs.view(N, T, self.topk, 1)
 
     def get_genotype(self):
         """ Return parameters to be optimized by ES. """
@@ -75,34 +73,82 @@ class SparseAttention(nn.Module):
 
 def unfold(batch, kernel=7, stride=4):
     """[summary]Extract patches (sliding window) from a batch of videos.
-   
+
     Args:
         batch (torch.tensor): Batch of videos of size N, T, C, W, H
     """
     N, T, C, W, H = batch.shape
     batch = F.unfold(batch.view(-1, C, W, H), kernel_size=kernel, stride=stride)
-    return batch.view(N, T, kernel ** 2 * 1, -1).transpose(2, 3)
+    return batch.view(N, T, kernel ** 2 * C, -1).transpose(2, 3)
+
+
+def conv_block(in_ch, out_ch):
+    return nn.Sequential(
+        nn.Conv2d(in_ch, out_ch, 5, bias=False),
+        nn.ReLU(inplace=True),
+        nn.BatchNorm2d(out_ch),
+        nn.Conv2d(out_ch, out_ch, 3, bias=False),
+        nn.ReLU(inplace=True),
+        nn.BatchNorm2d(out_ch),
+        nn.Conv2d(out_ch, out_ch, 3, bias=False),
+        nn.ReLU(inplace=True),
+        nn.BatchNorm2d(out_ch),
+    )
+
+
+class Head(nn.Module):
+    def __init__(self):
+        super(Head, self).__init__()
+        self.block1 = conv_block(1, 32)  # 3 conv layers
+        self.block2 = conv_block(32, 32)  # double it for greater good
+        self.conv1x1 = nn.Conv2d(
+            32, 1, 1, bias=False
+        )  # too many channels will kill you
+        self.bn = nn.BatchNorm2d(1)
+
+    def forward(self, x):
+        N, T, C, W, H = x.shape
+        x = x.view(-1, C, W, H)
+        x = self.conv1x1(self.block2(self.block1(x)))
+        x = self.bn(F.relu(x, inplace=True))
+        _, C, W, H = x.shape
+        return x.view(N, T, C, W, H)
 
 
 class Glimpsy(nn.Module):
-    def __init__(self, unfold, attention, rnn, linear):
-        super().__init__()
+    def __init__(self, unfold, attention, rnn, linear, head=None):
+        super(Glimpsy, self).__init__()
+        self.head = head
         self.unfold = unfold
         self.attention = attention
         self.rnn = rnn
+        self.bn = nn.BatchNorm1d(128)
         self.linear = linear
-        self._flat_params = None
 
-    def forward(self, input):
-        x = self.unfold(input)
-        x = self.attention(x)
+    def forward(self, x):
+        if self.head is not None:
+            # feature extractor
+            x = self.head(x)
+
+        # get patches
+        x = self.unfold(x)
+
+        # attention, no gradient required here
+        idxs = self.attention(x.data)
+
+        # use the idxs to reduce from n to k
+        N, T, n, d_in = x.shape
+        _, _, k, _ = idxs.shape
+        x = x.view(N * T, n, d_in)  # collapse batch and time
+        xg = x.gather(1, idxs.view(N * T, k, 1).expand(N * T, k, d_in))
 
         # ready for rnn
-        N, T, _, _ = x.shape
+        x = xg.view(N, T, k, d_in)  # unfold batch and time
         x = x.permute(1, 0, 2, 3)  # time first
         x = x.view(T, N, -1)  # collapse the local features
         _, (hn, _) = self.rnn(x)
-        return F.log_softmax(self.linear(hn.squeeze()), dim=1)
+        hn = self.bn(hn.squeeze())
+        return F.log_softmax(self.linear(hn), dim=1), xg, hn
 
     def get_genotype(self):
         return self.attention.get_genotype()
@@ -111,17 +157,35 @@ class Glimpsy(nn.Module):
         self.attention.set_genotype(parameters)
 
 
+def model_stats(model):
+    for mod_name, mod in zip(
+        ["CNN", "RNN", "BN", "Classifier"],
+        [model.head, model.rnn, model.bn, model.linear],
+    ):
+        print(f"{mod_name}: ---------")
+        for name, param in mod.named_parameters():
+            grad_mean = 42.424242
+            if param.grad is not None:
+                grad_mean = param.grad.data.mean()
+
+            print(
+                "{:16} mean={:6.5f}, var={:6.5f}. Grad: mean={:4.5f}".format(
+                    name, param.data.mean(), param.data.var(), grad_mean,
+                )
+            )
+
+
 def main():
     """ Entry point."""
-    epochs = 100
+    epochs = 200
     kernel_size = 7
     topk = 10
-    hidden_size = 64
+    hidden_size = 128
     num_labels = 46
     batch_size = 128
     popsize = 64
     device = torch.device("cuda")
-    experiment = f"{datetime.now().strftime('%b%d_%H%M%S')}_dev"
+    experiment = f"{datetime.now().strftime('%b%d_%H%M%S')}_rmsprop"
     path = f"./results/{experiment}"
 
     os.makedirs(path)
@@ -141,6 +205,7 @@ def main():
         SparseAttention(kernel_size ** 2, topk=topk, device=device),
         nn.LSTM(kernel_size ** 2 * topk, hidden_size=hidden_size, bias=False),
         nn.Linear(hidden_size, num_labels),
+        head=Head(),
     )
     model.to(device)
 
@@ -150,7 +215,7 @@ def main():
             torch.zeros((batch_size, 10, 1, 64, 64)).to(device),
             show_input=True,
             show_hierarchical=True,
-        )
+        ),
     )
 
     # not your grandma's optimizer
@@ -158,7 +223,16 @@ def main():
         model.get_genotype().numpy(), 0.1, {"popsize": popsize}
     )
     optim = O.Adam(model.parameters(), lr=0.001)
+    # optim = O.RMSprop(
+    #     model.parameters(),
+    #     lr=0.00025,
+    #     momentum=0.0,
+    #     alpha=0.95,
+    #     eps=0.01,
+    #     centered=True,
+    # )
 
+    gen_cnt, step_cnt = 0, 0
     for epoch in range(epochs):
         train_log.info(f"Epoch {epoch:03d} started ----------------------")
 
@@ -166,6 +240,7 @@ def main():
         correct = 0
         for idx, (data, target) in enumerate(loader):
             data, target = data.to(device), target.to(device)
+            data.requires_grad_(True)
 
             if (idx % popsize == 0) and (idx != 0):
                 es_optim.tell(candidates, losses)
@@ -176,16 +251,17 @@ def main():
                     "unFit": np.max(losses),
                     "attnMean": model.get_genotype().mean().item(),
                     "attnVar": model.get_genotype().var().item(),
-                    "gen": (idx // popsize),
+                    "gen": gen_cnt,
                 }
 
                 train_log.info(fmt.format(batch=idx, **stats))
-                train_log.trace(step=idx, **stats)
+                train_log.trace(step=step_cnt, **stats)
 
                 candidates, losses = es_optim.ask(), []
+                gen_cnt += 1
 
             model.set_genotype(candidates[idx % popsize])
-            ys = model(data)
+            ys, xg, hn = model(data)
             loss = F.nll_loss(ys, target.squeeze())
 
             optim.zero_grad()
@@ -196,6 +272,7 @@ def main():
             # stats
             pred = ys.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
+            step_cnt += 1
 
 
 if __name__ == "__main__":
